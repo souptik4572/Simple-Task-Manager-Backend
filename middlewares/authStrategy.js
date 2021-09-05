3a8cebf33440fc12c233a79388e014dfe7ac4f80
souptik4572 <souptiksarkar4572@gmail.com>
2021-09-05 14:40:44 +0530

    completed route protection for all routes

---
+++ middlewares/authStrategy.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authProtection = async (req, res, next) => {
+	const token = req.header('Authorization').split(' ')[1];
+	if (!token) {
+		return res.status(404).json({
+			success: false,
+			error: 'Access denied',
+		});
+	}
+	try {
+		const verifiedUser = jwt.verify(token, process.env.ACCESS_SECRET_TOKEN);
+		req.user = await User.findById(verifiedUser.userId);
+		next();
+	} catch (error) {
+		return res.status(404).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+module.exports = {
+	authProtection,
+};
