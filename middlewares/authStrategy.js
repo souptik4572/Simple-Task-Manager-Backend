c34a5222620e3f0b14f1fadb4cb657129bac2afc
souptik4572 <souptiksarkar4572@gmail.com>
2021-09-05 17:41:57 +0530

    fixed error handling for task model routing

--- middlewares/authStrategy.js
+++ middlewares/authStrategy.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const authProtection = async (req, res, next) => {
-	const token = req.header('Authorization').split(' ')[1];
+	const token = req.header('Authorization')?.split(' ')[1];
 	if (!token) {
 		return res.status(404).json({
 			success: false,
