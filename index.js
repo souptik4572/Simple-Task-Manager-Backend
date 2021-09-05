51e401c02bfd8d91def0c6e0c699f69465e74d07
souptik4572 <souptiksarkar4572@gmail.com>
2021-09-05 17:35:22 +0530

    completed entire routing for task model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const configureMongoose = require('./config/configureMongoose');
 const authenticationRoutes = require('./routes/authentication');
+const taskRoutes = require('./routes/task');
+const { authProtection } = require('./middlewares/authStrategy');
 
 // Configuring our mongoose connection
 configureMongoose();
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Our authentication routes
 app.use('/auth', authenticationRoutes);
+// Our task routes
+app.use('/task', authProtection, taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
