1e320deb531e6bf5459e03ff0024e6a96f7a5df2
souptik4572 <souptiksarkar4572@gmail.com>
2022-02-18 09:23:02 +0530

    add console statements for denugging JWT error

--- middlewares/authStrategy.js
+++ middlewares/authStrategy.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 
 const authProtection = async (req, res, next) => {
 	const token = req.header('Authorization')?.split(' ')[1];
+	console.log(token);
 	if (!token) {
 		return res.status(404).json({
 			success: false,
